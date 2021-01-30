bb7a971e1a8b5e850caea95ed22f30f8ee0bb433
Tarcisio Nunes <github@tarcnux.com.br>
2021-01-30 15:13:45 -0300

    Initial Spring Boot Project

---
+++ src/main/java/br/com/tarcnux/bdsClients/BdsClientsApplication.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux.bdsClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tarcnux/bdsClients/BdsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux.bdsClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
